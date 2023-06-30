d1b5793b564d7c6fafd09e1ed70db77abc731e5b
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-06-30 13:27:46 -0600

    Started implementing internal 'loopy' registers of the ppu. Continue by making getters and setters for the bit fields inside both vram address variables.

--- src/main/kotlin/ppu/GraphicsRenderer.kt
+++ src/main/kotlin/ppu/GraphicsRenderer.kt
@@ -3,8 +3,20 @@ package ppu
 @ExperimentalUnsignedTypes
 class GraphicsRenderer() {
 
-    private var currentVRAMAddress = UInt
-        set(value) { field = value and FIFTEEN_BIT_MASK}
+    var currentVRAMAddress: UInt = 0u
+        set(value) { field = value and FIFTEEN_BITMASK }
+
+    val currentCoarseXScroll: UInt
+        get() = currentVRAMAddress and COARSE_X_BITMASK
+
+
+    private var temporaryVRAMAddress: UInt = 0u
+        set(value) { field = value and FIFTEEN_BITMASK }
+
+    private var fineXScroll: UInt = 0u
+        set(value) { field = value and THREE_BITMASK }
+
+    private val firstOrSecondWriteToggle = false
 
     private val patternTableShiftRegister1 = ShiftRegister16Bit()
     private val patternTableShiftRegister2 = ShiftRegister16Bit()
@@ -13,7 +25,9 @@ class GraphicsRenderer() {
     private val paletteAttributeShiftRegister2 = ShiftRegister8Bit()
 
     companion object {
-        private val FIFTEEN_BIT_MASK = 0x8000u
+        private val FIFTEEN_BITMASK = 0x7FFFu
+        private val THREE_BITMASK = 0x7u
+        private val COARSE_X_BITMASK = 0x1Fu
     }
 
 }

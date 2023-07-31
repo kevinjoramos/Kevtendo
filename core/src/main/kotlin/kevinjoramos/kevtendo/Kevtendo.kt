dde7d861fed24655ae7b3bb2d3e8fc05bedb9045
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-07-30 18:02:59 -0600

    Learned how to use cameras and viewports to center the game screen and resize properly. Should be good to start connection the new interface.

--- core/src/main/kotlin/kevinjoramos/kevtendo/Kevtendo.kt
+++ core/src/main/kotlin/kevinjoramos/kevtendo/Kevtendo.kt
@@ -2,10 +2,14 @@ package kevinjoramos.kevtendo
 
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 
@@ -13,9 +17,16 @@ import ktx.app.KtxScreen
 class Kevtendo : KtxGame<KtxScreen>() {
 
     private lateinit var spriteBatch: SpriteBatch
+    private lateinit var viewport: Viewport
+    private lateinit var camera: Camera
 
     override fun create() {
         super.create()
+
+        camera = OrthographicCamera()
+        viewport = FitViewport(256f, 240f, camera)
+        viewport.apply()
+        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f)
 
         Gdx.app.logLevel = Application.LOG_DEBUG
 
@@ -23,22 +34,31 @@ class Kevtendo : KtxGame<KtxScreen>() {
         //setScreen<GameScreen>()
 
         spriteBatch = SpriteBatch()
+    }
 
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
 
+        viewport.update(width, height)
+        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f)
     }
 
     override fun render() {
         super.render()
 
+        camera.update()
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        val noise = Array(240) { IntArray(256) { if ((0..1).random() == 0) 0 else 0xFFFFFFF } }
+        spriteBatch.projectionMatrix = camera.combined
+
+        val noise = Array(240) { UIntArray(256) { if ((0..1).random() == 0) 0u else 0xFFFFFFFFu} }
 
         val pixels = Pixmap(256, 240, Pixmap.Format.RGBA8888)
 
         for ((i, row) in noise.withIndex()) {
             for ((j, pixel) in row.withIndex()) {
-                pixels.drawPixel(j, i, pixel)
+                pixels.drawPixel(j, i, pixel.toInt())
             }
         }
 

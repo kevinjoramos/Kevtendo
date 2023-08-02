d4b3b19a688c2eab732174f23dd9046df310e514
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-01 22:18:52 -0600

    Fixed framerate to be under 60fps.

--- core/src/main/kotlin/NesEmulator/ui/Colors.kt
+++ core/src/main/kotlin/NesEmulator/ui/Colors.kt
@@ -5,68 +5,136 @@ object NesColors {
     //val LightGreen100 = Color(0xff1f5150)
 
 
-    const val COLOR_00 = 0x626262FF
-    const val COLOR_01 = 0x0D226BFF
-    const val COLOR_02 = 0x241476FF
-    const val COLOR_03 = 0x3B0A6BFF
-    const val COLOR_04 = 0x4C074DFF
-    const val COLOR_05 = 0x520C24FF
-    const val COLOR_06 = 0x4C1700FF
-    const val COLOR_07 = 0x3B2600FF
-    const val COLOR_08 = 0x243400FF
-    const val COLOR_09 = 0x0D3D00FF
-    const val COLOR_0A = 0x227C15FF
-    const val COLOR_0B = 0x003B24FF
-    const val COLOR_0C = 0x00304DFF
-    const val COLOR_0D = 0x000000FF
-    const val COLOR_0E = 0x000000FF
-    const val COLOR_0F = 0x000000FF
-    const val COLOR_10 = 0xABABABFF
-    const val COLOR_11 = 0x3156B1FF
-    const val COLOR_12 = 0x5043C5FF
-    const val COLOR_13 = 0x7034BBFF
-    const val COLOR_14 = 0x892F95FF
-    const val COLOR_15 = 0x94345FFF
-    const val COLOR_16 = 0x8E4226FF
-    const val COLOR_17 = 0x795500FF
-    const val COLOR_18 = 0x5B6800FF
-    const val COLOR_19 = 0x3B7700FF
-    const val COLOR_1A = 0x227C15FF
-    const val COLOR_1B = 0x17774CFF
-    const val COLOR_1C = 0x1D6985FF
-    const val COLOR_1D = 0x000000FF
-    const val COLOR_1E = 0x000000FF
-    const val COLOR_1F = 0x000000FF
-    const val COLOR_20 = 0xFFFFFFFF
-    const val COLOR_21 = 0x7CAAFFFF
-    const val COLOR_22 = 0x9B96FFFF
-    const val COLOR_23 = 0xBD86FFFF
-    const val COLOR_24 = 0xD87EF1FF
-    const val COLOR_25 = 0xE682BAFF
-    const val COLOR_26 = 0xE38F7FFF
-    const val COLOR_27 = 0xD0A24EFF
-    const val COLOR_28 = 0xB2B734FF
-    const val COLOR_29 = 0x90C739FF
-    const val COLOR_2A = 0x74CE5CFF
-    const val COLOR_2B = 0x66CB92FF
-    const val COLOR_2C = 0x69BECEFF
-    const val COLOR_2D = 0x4E4E4EFF
-    const val COLOR_2E = 0x000000FF
-    const val COLOR_2F = 0x000000FF
-    const val COLOR_30 = 0xFFFFFFFF
-    const val COLOR_31 = 0xC9DEFCFF
-    const val COLOR_32 = 0xD5D6FFFF
-    const val COLOR_33 = 0xE2CFFFFF
-    const val COLOR_34 = 0xEECCFCFF
-    const val COLOR_35 = 0xF5CCE7FF
-    const val COLOR_36 = 0xF5D1CFFF
-    const val COLOR_37 = 0xEED8BBFF
-    const val COLOR_38 = 0xE2E1AEFF
-    const val COLOR_39 = 0xD5E8AEFF
-    const val COLOR_3A = 0xC9EBBBFF
-    const val COLOR_3B = 0xC2EBCFFF
-    const val COLOR_3C = 0xC2E6E7FF
-    const val COLOR_3D = 0xB8B8B8FF
-    const val COLOR_3E = 0x000000FF
-    const val COLOR_3F = 0x000000FF
+    private const val COLOR_00 = 0x626262FFu
+    private const val COLOR_01 = 0x0D226BFFu
+    private const val COLOR_02 = 0x241476FFu
+    private const val COLOR_03 = 0x3B0A6BFFu
+    private const val COLOR_04 = 0x4C074DFFu
+    private const val COLOR_05 = 0x520C24FFu
+    private const val COLOR_06 = 0x4C1700FFu
+    private const val COLOR_07 = 0x3B2600FFu
+    private const val COLOR_08 = 0x243400FFu
+    private const val COLOR_09 = 0x0D3D00FFu
+    private const val COLOR_0A = 0x227C15FFu
+    private const val COLOR_0B = 0x003B24FFu
+    private const val COLOR_0C = 0x00304DFFu
+    private const val COLOR_0D = 0x000000FFu
+    private const val COLOR_0E = 0x000000FFu
+    private const val COLOR_0F = 0x000000FFu
+    private const val COLOR_10 = 0xABABABFFu
+    private const val COLOR_11 = 0x3156B1FFu
+    private const val COLOR_12 = 0x5043C5FFu
+    private const val COLOR_13 = 0x7034BBFFu
+    private const val COLOR_14 = 0x892F95FFu
+    private const val COLOR_15 = 0x94345FFFu
+    private const val COLOR_16 = 0x8E4226FFu
+    private const val COLOR_17 = 0x795500FFu
+    private const val COLOR_18 = 0x5B6800FFu
+    private const val COLOR_19 = 0x3B7700FFu
+    private const val COLOR_1A = 0x227C15FFu
+    private const val COLOR_1B = 0x17774CFFu
+    private const val COLOR_1C = 0x1D6985FFu
+    private const val COLOR_1D = 0x000000FFu
+    private const val COLOR_1E = 0x000000FFu
+    private const val COLOR_1F = 0x000000FFu
+    private const val COLOR_20 = 0xFFFFFFFFu
+    private const val COLOR_21 = 0x7CAAFFFFu
+    private const val COLOR_22 = 0x9B96FFFFu
+    private const val COLOR_23 = 0xBD86FFFFu
+    private const val COLOR_24 = 0xD87EF1FFu
+    private const val COLOR_25 = 0xE682BAFFu
+    private const val COLOR_26 = 0xE38F7FFFu
+    private const val COLOR_27 = 0xD0A24EFFu
+    private const val COLOR_28 = 0xB2B734FFu
+    private const val COLOR_29 = 0x90C739FFu
+    private const val COLOR_2A = 0x74CE5CFFu
+    private const val COLOR_2B = 0x66CB92FFu
+    private const val COLOR_2C = 0x69BECEFFu
+    private const val COLOR_2D = 0x4E4E4EFFu
+    private const val COLOR_2E = 0x000000FFu
+    private const val COLOR_2F = 0x000000FFu
+    private const val COLOR_30 = 0xFFFFFFFFu
+    private const val COLOR_31 = 0xC9DEFCFFu
+    private const val COLOR_32 = 0xD5D6FFFFu
+    private const val COLOR_33 = 0xE2CFFFFFu
+    private const val COLOR_34 = 0xEECCFCFFu
+    private const val COLOR_35 = 0xF5CCE7FFu
+    private const val COLOR_36 = 0xF5D1CFFFu
+    private const val COLOR_37 = 0xEED8BBFFu
+    private const val COLOR_38 = 0xE2E1AEFFu
+    private const val COLOR_39 = 0xD5E8AEFFu
+    private const val COLOR_3A = 0xC9EBBBFFu
+    private const val COLOR_3B = 0xC2EBCFFFu
+    private const val COLOR_3C = 0xC2E6E7FFu
+    private const val COLOR_3D = 0xB8B8B8FFu
+    private const val COLOR_3E = 0x000000FFu
+    private const val COLOR_3F = 0x000000FFu
+
+    fun decodeNesColor(encodedColorHex: Int): Int =
+        when (encodedColorHex) {
+            0x00 -> COLOR_00
+            0x01 -> COLOR_01
+            0x02 -> COLOR_02
+            0x03 -> COLOR_03
+            0x04 -> COLOR_04
+            0x05 -> COLOR_05
+            0x06 -> COLOR_06
+            0x07 -> COLOR_07
+            0x08 -> COLOR_08
+            0x09 -> COLOR_09
+            0x0A -> COLOR_0A
+            0x0B -> COLOR_0B
+            0x0C -> COLOR_0C
+            0x0D -> COLOR_0D
+            0x0E -> COLOR_0E
+            0x0F -> COLOR_0F
+            0x10 -> COLOR_10
+            0x11 -> COLOR_11
+            0x12 -> COLOR_12
+            0x13 -> COLOR_13
+            0x14 -> COLOR_14
+            0x15 -> COLOR_15
+            0x16 -> COLOR_16
+            0x17 -> COLOR_17
+            0x18 -> COLOR_18
+            0x19 -> COLOR_19
+            0x1A -> COLOR_1A
+            0x1B -> COLOR_1B
+            0x1C -> COLOR_1C
+            0x1D -> COLOR_1D
+            0x1E -> COLOR_1E
+            0x1F -> COLOR_1F
+            0x20 -> COLOR_20
+            0x21 -> COLOR_21
+            0x22 -> COLOR_22
+            0x23 -> COLOR_23
+            0x24 -> COLOR_24
+            0x25 -> COLOR_25
+            0x26 -> COLOR_26
+            0x27 -> COLOR_27
+            0x28 -> COLOR_28
+            0x29 -> COLOR_29
+            0x2A -> COLOR_2A
+            0x2B -> COLOR_2B
+            0x2C -> COLOR_2C
+            0x2D -> COLOR_2D
+            0x2E -> COLOR_2E
+            0x2F -> COLOR_1F
+            0x30 -> COLOR_30
+            0x31 -> COLOR_31
+            0x32 -> COLOR_32
+            0x33 -> COLOR_33
+            0x34 -> COLOR_34
+            0x35 -> COLOR_35
+            0x36 -> COLOR_36
+            0x37 -> COLOR_37
+            0x38 -> COLOR_38
+            0x39 -> COLOR_39
+            0x3A -> COLOR_3A
+            0x3B -> COLOR_3B
+            0x3C -> COLOR_3C
+            0x3D -> COLOR_3D
+            0x3E -> COLOR_3E
+            else -> COLOR_3F
+        }.toInt()
 }
--- core/src/main/kotlin/kevinjoramos/kevtendo/Kevtendo.kt
+++ core/src/main/kotlin/kevinjoramos/kevtendo/Kevtendo.kt
@@ -13,7 +13,9 @@ import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
+import ktx.log.debug
 import ui.NesColors
+import kotlin.system.measureTimeMillis
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
 @OptIn(ExperimentalUnsignedTypes::class)
@@ -71,16 +73,14 @@ class Kevtendo : KtxGame<KtxScreen>() {
     override fun render() {
         super.render()
 
+        // Run emulation for one frame.
         executeFrameCycle()
-
         systemClock = 0
 
         camera.update()
-
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         spriteBatch.projectionMatrix = camera.combined
-
         val pixels = Pixmap(256, 240, Pixmap.Format.RGBA8888)
 
         for ((i, row) in bus.ppu.frameBuffer.withIndex()) {
@@ -88,72 +88,7 @@ class Kevtendo : KtxGame<KtxScreen>() {
                 pixels.drawPixel(
                     j,
                     i,
-                    when (pixel.toUInt()) {
-                        0x00u -> NesColors.COLOR_00
-                        0x01u -> NesColors.COLOR_01
-                        0x02u -> NesColors.COLOR_02
-                        0x03u -> NesColors.COLOR_03
-                        0x04u -> NesColors.COLOR_04
-                        0x05u -> NesColors.COLOR_05
-                        0x06u -> NesColors.COLOR_06
-                        0x07u -> NesColors.COLOR_07
-                        0x08u -> NesColors.COLOR_08
-                        0x09u -> NesColors.COLOR_09
-                        0x0Au -> NesColors.COLOR_0A
-                        0x0Bu -> NesColors.COLOR_0B
-                        0x0Cu -> NesColors.COLOR_0C
-                        0x0Du -> NesColors.COLOR_0D
-                        0x0Eu -> NesColors.COLOR_0E
-                        0x0Fu -> NesColors.COLOR_0F
-                        0x10u -> NesColors.COLOR_10
-                        0x11u -> NesColors.COLOR_11
-                        0x12u -> NesColors.COLOR_12
-                        0x13u -> NesColors.COLOR_13
-                        0x14u -> NesColors.COLOR_14
-                        0x15u -> NesColors.COLOR_15
-                        0x16u -> NesColors.COLOR_16
-                        0x17u -> NesColors.COLOR_17
-                        0x18u -> NesColors.COLOR_18
-                        0x19u -> NesColors.COLOR_19
-                        0x1Au -> NesColors.COLOR_1A
-                        0x1Bu -> NesColors.COLOR_1B
-                        0x1Cu -> NesColors.COLOR_1C
-                        0x1Du -> NesColors.COLOR_1D
-                        0x1Eu -> NesColors.COLOR_1E
-                        0x1Fu -> NesColors.COLOR_1F
-                        0x20u -> NesColors.COLOR_20
-                        0x21u -> NesColors.COLOR_21
-                        0x22u -> NesColors.COLOR_22
-                        0x23u -> NesColors.COLOR_23
-                        0x24u -> NesColors.COLOR_24
-                        0x25u -> NesColors.COLOR_25
-                        0x26u -> NesColors.COLOR_26
-                        0x27u -> NesColors.COLOR_27
-                        0x28u -> NesColors.COLOR_28
-                        0x29u -> NesColors.COLOR_29
-                        0x2Au -> NesColors.COLOR_2A
-                        0x2Bu -> NesColors.COLOR_2B
-                        0x2Cu -> NesColors.COLOR_2C
-                        0x2Du -> NesColors.COLOR_2D
-                        0x2Eu -> NesColors.COLOR_2E
-                        0x2Fu -> NesColors.COLOR_1F
-                        0x30u -> NesColors.COLOR_30
-                        0x31u -> NesColors.COLOR_31
-                        0x32u -> NesColors.COLOR_32
-                        0x33u -> NesColors.COLOR_33
-                        0x34u -> NesColors.COLOR_34
-                        0x35u -> NesColors.COLOR_35
-                        0x36u -> NesColors.COLOR_36
-                        0x37u -> NesColors.COLOR_37
-                        0x38u -> NesColors.COLOR_38
-                        0x39u -> NesColors.COLOR_39
-                        0x3Au -> NesColors.COLOR_3A
-                        0x3Bu -> NesColors.COLOR_3B
-                        0x3Cu -> NesColors.COLOR_3C
-                        0x3Du -> NesColors.COLOR_3D
-                        0x3Eu -> NesColors.COLOR_3E
-                        else -> NesColors.COLOR_3F
-                    }.toInt()
+                    NesColors.decodeNesColor(pixel.toInt())
                 )
             }
         }
@@ -167,6 +102,11 @@ class Kevtendo : KtxGame<KtxScreen>() {
         spriteBatch.end()
 
         frameBuffer.dispose()
+
+        // Limit the frame rate to 60 fps.
+        while (Gdx.graphics.deltaTime < (1 / 60)) {
+            continue
+        }
     }
 
     private fun executeFrameCycle() {
